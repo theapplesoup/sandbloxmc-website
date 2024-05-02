6a03225d28b538b3dd3c6beabbbe778a159e10be
theapplesoup <theapplesoup@gmail.com>
2024-05-02 16:23:40 -0400

    Update website

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("myBtn").addEventListener("click", () => setClipboard("sandbloxmc.net"));
+
+async function setClipboard(text) {
+  const type = "text/plain";
+  const blob = new Blob([text], { type });
+  const data = [new ClipboardItem({ [type]: blob })];
+  await navigator.clipboard.write(data);
+  alert("Copied to clipboard!");
+}
